cde51ea64ebf9678ce3636fb250437e1b77bc3fb
Fernando Ribas <fribas84@gmail.com>
2023-12-14 11:16:27 -0300

    sqllite for initial dev

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -4,9 +4,19 @@ import { AppService } from './app.service';
 import { WalletsModule } from './wallets/wallets.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [WalletsModule, UsersModule, AuthModule],
+  imports: [
+    WalletsModule,
+    UsersModule,
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
