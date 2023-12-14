bd3ba9910fb3c0b3b0abfb8ba5db9f4aa527bb61
Fernando Ribas <fribas84@gmail.com>
2023-12-14 10:27:31 -0300

    auth controller setup

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WalletsModule } from './wallets/wallets.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [WalletsModule, UsersModule],
+  imports: [WalletsModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
