64413cf9b196084f4a34cc8909935a4fdc7de942
Fernando Ribas <fribas84@gmail.com>
2023-12-14 10:11:27 -0300

    nest resource files

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WalletsModule } from './wallets/wallets.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [WalletsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
