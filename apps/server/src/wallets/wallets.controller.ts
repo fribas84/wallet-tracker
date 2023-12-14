139abefccfc2bae3d3ac911ca43d11948f72a41f
Fernando Ribas <fribas84@gmail.com>
2023-12-14 10:35:03 -0300

    class validator + class transformer

--- apps/server/src/wallets/wallets.controller.ts
+++ apps/server/src/wallets/wallets.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('wallets')
-export class WalletsController {}
+export class WalletsController {
+  @Get('/:id')
+  getWallets(@Param('id') id: string) {
+    return 'get all wallets of user id: ' + id;
+  }
+
+    @Post('/:id')
+    
+}
