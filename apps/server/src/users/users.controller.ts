113ceab7d21e39f3b5009bdb051e1123141c0442
Fernando Ribas <fribas84@gmail.com>
2023-12-15 11:19:17 -0300

    TODO

--- apps/server/src/users/users.controller.ts
+++ apps/server/src/users/users.controller.ts
@@ -6,11 +6,14 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { AuthGuard } from './auth.guard';
 
+//TODO implement better the authguard  using passport
 @Controller('auth')
 export class UsersController {
   constructor(private readonly usersService: UsersService) { }
@@ -40,6 +43,7 @@ export class UsersController {
     return this.usersService.findById(+id);
   }
 
+  @UseGuards(AuthGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() body: UpdateUserDto) {
     return this.usersService.update(+id, body);
