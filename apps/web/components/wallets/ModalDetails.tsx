6f39259d1387e2a94839ceeffdaddedc2a0d2fcb
Fernando Ribas <fribas84@gmail.com>
2023-12-18 01:02:01 -0300

    clean whitespace

--- apps/web/components/wallets/ModalDetails.tsx
+++ apps/web/components/wallets/ModalDetails.tsx
@@ -49,7 +49,6 @@ const ModalDetails = ({ name, id, showModalDetails, setShowModalDetails }: Props
         <>
             {showModalDetails && (
                 <>
-
                     <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
 
                         <div className="relative my-6 mx-auto w-3/4">
