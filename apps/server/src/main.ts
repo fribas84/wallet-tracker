7b549aa5b86be97e57203140544c103be46acbe6
Fernando Ribas <fribas84@gmail.com>
2023-12-20 09:37:21 -0300

    bootsrap loggers

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
+
+  logger.log('Starting application...');
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.setGlobalPrefix('api/v1');
-
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Wallet Tracker')
@@ -19,6 +22,9 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT || 4000);
+  logger.log('Application started');
+  logger.log(`Listening on port ${process.env.PORT || 4000}`);
+  logger.log(await app.getUrl());
 }
 
 bootstrap();
