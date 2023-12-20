d9132bef2983308ad4f3fb3ec210689d02b81ccc
Fernando Ribas <fribas84@gmail.com>
2023-12-20 09:26:48 -0300

    port

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.SERVER_PORT || 4000);
+  await app.listen(process.env.PORT || 4000);
 }
 
 bootstrap();
